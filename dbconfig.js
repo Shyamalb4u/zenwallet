13f113c1822c0afbd729166413b11d10aee5b718
Shyamal <info@bigsss.com>
2025-04-11 16:20:15 +0530

    Database server changed

--- dbconfig.js
+++ dbconfig.js
@@ -3,7 +3,7 @@ const { prototype } = require("jsonwebtoken/lib/JsonWebTokenError");
 const config = {
   user: "zenwallet_user", // Database username
   password: "ZWd597Cn*Dat", // Database password
-  server: "103.69.196.81", // Server IP address
+  server: "78.47.118.224", // Server IP address
   database: "zenwallet_data", // Database name
   options: {
     encrypt: false, // Disable encryption
